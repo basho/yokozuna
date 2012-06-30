9eecfab67474d6c8421f60cd41d5d937db92c718
Ryan Zezeski <rzezeski@gmail.com>
2012-06-30 19:30:12 -0400

    Replace crappy build text with slightly less crappy script

---
+++ priv/build-jar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Temporary script to build JAR file containing customer Solr request
+# handlers.
+
+SOLR_JAR_DIR=${SOLR_JAR_DIR:-'/usr/local/apache-solr-3.5.0/example/work/Jetty_0_0_0_0_8983_solr.war__solr__k1kf17/webapp/WEB-INF/lib/*'}
+
+javac -cp "$SOLR_JAR_DIR" java/com/basho/yokozuna/handler/MerkleTreeHandler.java
+jar cvf yokozuna.jar -C java/ .
+
+mkdir java_lib
+cp yokozuna.jar java_lib
