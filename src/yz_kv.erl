2dece5af76a6bbe29d34428c355072ffe1b35dac
Ryan Zezeski <rzezeski@gmail.com>
2012-09-04 11:08:53 -0400

    Wait for index creation on first handoff write
    When a new node joins the cluster there is a race between handoff and
    index creation.  The index creation is triggered by a ring event and
    may take several seconds to complete.  Handoff is triggered by a ring
    event and the vnode manager tick.  If using a forced join the handoff
    can start quick enough that the new node hasn't finished creating all
    the indexes.  If an index isn't ready then the hook will fail to index
    the object.  Anti-entropy will converge the index with the object but
    that may not happen immediately.
    
    This solution waits for the index on the first handoff write for up to
    `max_wait_for_index_seconds`.  This wait will only occur once per
    vnode regardless if the index is ready or not after the max time has
    expired.  In that case object indexing will fail until the index is
    ready just as it did before this patch.  This simply gives the index a
    chance to catch up.
    
    In the best case the index will be ready before max wait and all
    objects will be indexed correctly.  In the unfortunate case the index
    will take a little longer and some objects will fail to index but
    anti-entropy will fix that at some point.  In the worst case the index
    will completely fail and all objects will fail to index.  This is
    desired behavior because even though the indexes won't exist the raw
    data is still making it to disk.  Once the problem with the index is
    solved anti-entropy will create the indexes that were missed.

--- src/yz_kv.erl
+++ src/yz_kv.erl
@@ -25,13 +25,28 @@
 -compile(export_all).
 -include("yokozuna.hrl").
 
+-define(ONE_SECOND, 1000).
+-define(WAIT_FLAG(Index), {wait_flag, Index}).
+-define(MAX_WAIT_FOR_INDEX, app_helper:get_env(?YZ_APP_NAME, max_wait_for_index_seconds, 5)).
+
+-type check() :: {module(), atom(), list()}.
+-type seconds() :: pos_integer().
+-type write_reason() :: handoff | put.
+
+
 %%%===================================================================
 %%% API
 %%%===================================================================
 
-index_obj(Obj, VNodeState) ->
+%% @doc An object modified hook to create indexes as object data is
+%% written or modified.
+%%
+%% NOTE: This code runs on the vnode process.
+-spec index(riak_object:riak_object(), write_reason(), term()) -> ok.
+index(Obj, Reason, VNodeState) ->
     {ok, Ring} = riak_core_ring_manager:get_my_ring(),
     {Bucket, _} = BKey = {riak_object:bucket(Obj), riak_object:key(Obj)},
+    ok = maybe_wait(Reason, Bucket),
     BProps = riak_core_bucket:get_bucket(Bucket, Ring),
     NVal = riak_core_bucket:n_val(BProps),
     Idx = riak_core_util:chash_key(BKey),
@@ -39,11 +54,13 @@ index_obj(Obj, VNodeState) ->
     FPN = ?INT_TO_BIN(first_partition(IdealPreflist)),
     Partition = ?INT_TO_BIN(get_partition(VNodeState)),
     Doc = yz_doc:make_doc(Obj, FPN, Partition),
-    yz_solr:index(binary_to_list(Bucket), [Doc]).
+    ok = yz_solr:index(binary_to_list(Bucket), [Doc]).
 
-install_obj_modified_hook(Bucket) when is_binary(Bucket) ->
+%% @doc Install the object modified hook on the given `Bucket'.
+-spec install_hook(binary()) -> ok.
+install_hook(Bucket) when is_binary(Bucket) ->
     Mod = yz_kv,
-    Fun = index_obj,
+    Fun = index,
     ok = riak_kv_vnode:add_obj_modified_hook(Bucket, Mod, Fun).
 
 
@@ -51,8 +68,58 @@ install_obj_modified_hook(Bucket) when is_binary(Bucket) ->
 %%% Private
 %%%===================================================================
 
+%% @private
+%%
+%% @doc Determine whether process `Flag' is set.
+-spec check_flag(term()) -> boolean().
+check_flag(Flag) ->
+    true == erlang:get(Flag).
+
+%% @private
+%%
+%% @doc Get first partition from a preflist.
 first_partition([{Partition, _}|_]) ->
     Partition.
 
+%% @private
+%%
+%% @doc Get the partition from the `VNodeState'.
 get_partition(VNodeState) ->
     riak_kv_vnode:get_state_partition(VNodeState).
+
+%% @private
+%%
+%% @doc Wait for index creation if hook was invoked for handoff write.
+-spec maybe_wait(write_reason(), binary()) -> ok.
+maybe_wait(handoff, Bucket) ->
+    Index = binary_to_list(Bucket),
+    Flag = ?WAIT_FLAG(Index),
+    case check_flag(Flag) of
+        false ->
+            Seconds = ?MAX_WAIT_FOR_INDEX,
+            ok = wait_for({yz_solr, ping, [Index]}, Seconds),
+            ok = set_flag(Flag);
+        true ->
+            ok
+    end;
+maybe_wait(_, _) ->
+    ok.
+
+%% @doc Set the `Flag'.
+-spec set_flag(term()) -> ok.
+set_flag(Flag) ->
+    erlang:put(Flag, true),
+    ok.
+
+%% @doc Wait for `Check' for the given number of `Seconds'.
+-spec wait_for(check(), seconds()) -> ok.
+wait_for(_, 0) ->
+    ok;
+wait_for(Check={M,F,A}, Seconds) when Seconds > 0 ->
+    case M:F(A) of
+        true ->
+            ok;
+        false ->
+            timer:sleep(?ONE_SECOND),
+            wait_for(Check, Seconds - 1)
+    end.
