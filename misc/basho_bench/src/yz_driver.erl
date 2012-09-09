0491b647feefab363f22124990e847f15aa72e88
Ryan Zezeski <rzezeski@gmail.com>
2012-09-09 14:29:41 -0400

    Add basho bench driver
    These modules are slightly modified versions of the ones I was using
    to bench Riak Search.  Some things may be broken or still rely on Riak
    Search.

---
+++ misc/basho_bench/src/yz_driver.erl
@@ -0,0 +1,234 @@
+%% @doc A Basho Bench driver for Yokozuna.
+-module(yz_driver).
+
+%% Callbacks
+-export([new/1,
+         run/4]).
+
+%% Key Gens
+-export([always/2,
+         fruit_key_val_gen/1,
+         fruit_key_val_gen/2,
+         valgen/4,
+         valgen_i/1]).
+
+-include_lib("basho_bench/include/basho_bench.hrl").
+-record(state, {pb_conns, index, iurls, surls}).
+-define(DONT_VERIFY, dont_verify).
+
+%% ====================================================================
+%% API
+%% ====================================================================
+
+new(_Id) ->
+    ibrowse:start(),
+    Index = basho_bench_config:get(index, "test"),
+    HTTP = basho_bench_config:get(http_conns, [{"127.0.0.1", 8098}]),
+    PB = basho_bench_config:get(pb_conns, [{"127.0.0.1", 8087}]),
+    IPath = basho_bench_config:get(index_path, "/riak/test"),
+    SPath = basho_bench_config:get(search_path, "/search/test"),
+    IURLs = array:from_list(lists:map(make_url(IPath), HTTP)),
+    SURLs = array:from_list(lists:map(make_url(SPath), HTTP)),
+    Conns = array:from_list(lists:map(fun make_conn/1, PB)),
+    N = length(HTTP),
+    M = length(PB),
+
+    {ok, #state{pb_conns={Conns, {0,M}},
+                index=list_to_binary(Index),
+                iurls={IURLs, {0,N}},
+                surls={SURLs, {0,N}}}}.
+
+run(search, _KeyGen, ValGen, S=#state{surls=URLs}) ->
+    Base = get_base(URLs),
+    {Field, [Term]} = ValGen(search),
+    Qry = ?FMT("~s:~s", [Field, Term]),
+    Params = mochiweb_util:urlencode([{<<"q">>, Qry}]),
+    URL = ?FMT("~s?~s", [Base, Params]),
+    S2 = S#state{surls=wrap(URLs)},
+    case http_get(URL) of
+        {ok, _} -> {ok, S2};
+        {error, Reason} -> {error, Reason, S2}
+    end;
+
+run({search, Qry, FL}, KG, VG, S) ->
+    run({search, Qry, FL, ?DONT_VERIFY}, KG, VG, S);
+
+run({search, Qry, FL, Expected}, _, _, S=#state{surls=URLs}) ->
+    Base = get_base(URLs),
+    Params = mochiweb_util:urlencode([{q, Qry}, {wt, <<"json">>}, {fl, FL}]),
+    URL = ?FMT("~s?~s", [Base, Params]),
+    S2 = S#state{surls=wrap(URLs)},
+    case {Expected, http_get(URL)} of
+        {?DONT_VERIFY, {ok,_}} ->
+            {ok, S2};
+        {_, {ok, Body}} ->
+            check_numfound(Qry, Body, Expected, S2);
+        {_, {error, Reason}} ->
+            {error, Reason, S2}
+    end;
+
+run(index, _KeyGen, ValGen, S=#state{iurls=URLs}) ->
+    Base = get_base(URLs),
+    {Key, Line} = ValGen(index),
+    URL = ?FMT("~s/~s", [Base, Key]),
+    S2 = S#state{iurls=wrap(URLs)},
+    case http_put(URL, "application/json", Line) of
+        ok -> {ok, S2};
+        {error, Reason} -> {error, Reason, S2}
+    end;
+
+run(load_fruit, KeyValGen, _, S=#state{iurls=URLs}) ->
+    Base = get_base(URLs),
+    {Key, Val} = KeyValGen(),
+    URL = ?FMT("~s/~p", [Base, Key]),
+    S2 = S#state{iurls=wrap(URLs)},
+    case http_put(URL, "plain/text", Val) of
+        ok -> {ok, S2};
+        {error, Reason} -> {error, Reason, S2}
+    end;
+
+run(search_pb, _, QueryGen, S=#state{index=Index, pb_conns=Conns}) ->
+    Conn = get_conn(Conns),
+    Query = QueryGen(),
+    S2 = S#state{pb_conns=wrap(Conns)},
+    case search_pb(Conn, Index, Query) of
+        ok -> {ok, S2};
+        {error, Reason} -> {error, Reason, S2}
+    end;
+
+run(show, KeyGen, _ValGen, S) ->
+    {K, V} = KeyGen(),
+    ?INFO("~p: ~p~n", [K, V]),
+    {ok, S}.
+
+search_pb(Conn, Index, Query) ->
+    case riakc_pb_socket:search(Conn, Index, Query) of
+        {ok, _Result} -> ok;
+        Other -> Other
+    end.
+
+%% ====================================================================
+%% Key Gens
+%% ====================================================================
+
+%% @doc Allow to have different valgen depending on operation.
+valgen(Id, Path, Fields, Schema) ->
+    N = basho_bench_config:get(concurrent),
+    if Id == N ->
+            {ok, _} = yz_file_terms:start_ts(Path, Fields, Schema),
+            {ok, _} = yz_file_terms:start_ls(Path);
+       true -> ok
+    end,
+    fun ?MODULE:valgen_i/1.
+
+valgen_i(index) ->
+    yz_file_terms:get_line();
+valgen_i(search) ->
+    yz_file_terms:get_ft().
+
+-define(K100, 100000).
+-define(K10, 10000).
+-define(K1, 1000).
+
+-define(FRUITS,
+        [{?K100, "apple grape orange pineapple strawberry kiwi"},
+         {?K10, "avocado raspberry persimmon blackberry cherry tomato"},
+         {?K1, "clementine lime lemon melon plum pear"},
+         {100, "marang nutmeg olive pecan peanut tangerine"},
+         {10, "nunga nance mulberry langsat karonda kumquat"},
+         {1, "korlan jocote genip elderberry citron jujube"}]).
+
+%% Deal with off-by-one scenario, sequential-key-gen doesn't include
+%% max value.
+-define(LAST_ONE(Concurrent, Id),
+        if Concurrent == Id -> 1;
+           true -> 0
+        end).
+
+%% generates key and value because value is based on key
+fruit_key_val_gen(Id) ->
+    fruit_key_val_gen(Id, ?K100).
+
+fruit_key_val_gen(Id, NumKeys) ->
+    Fruits2 = [{N, combine(?FRUITS, N)} || N <- [1, 10, 100, ?K1, ?K10, ?K100]],
+    StartKey = 1,
+    Workers = basho_bench_config:get(concurrent),
+    Range = NumKeys div Workers,
+    MinValue = StartKey + Range * (Id - 1),
+    MaxValue = StartKey + Range * Id,
+    Ref = make_ref(),
+    ?DEBUG("ID ~p generating range ~p to ~p\n", [Id, MinValue, MaxValue]),
+    fun() ->
+            K = basho_bench_keygen:sequential_int_generator(Ref, Range + ?LAST_ONE(Workers, Id), Id) + MinValue,
+            V = first_large_enough(K, Fruits2),
+            {K, V}
+    end.
+
+always(_Id, Val) ->
+    fun() -> Val end.
+
+%% ====================================================================
+%% Private
+%% ====================================================================
+
+-spec check_numfound(binary(), binary(), integer(), #state{}) ->
+                            {ok, #state{}} | {error, any(), #state{}}.
+check_numfound(Qry, Body, Expected, S) ->
+    Struct = mochijson2:decode(Body),
+    NumFound = get_path(Struct, [<<"response">>, <<"numFound">>]),
+    case Expected == NumFound of
+        true ->
+            {ok, S};
+        false ->
+            ?ERROR("Query ~p expected ~p got ~p~nBody: ~s~n",
+                   [Qry, Expected, NumFound, Body]),
+            {error, {num_found, Expected, NumFound}, S}
+    end.
+
+combine(Fruits, N) ->
+    string:join([Str || {Count, Str} <- Fruits, Count >= N], " ").
+
+first_large_enough(K, [{Count, Str}|Fruits]) ->
+    if Count >= K -> Str;
+       true -> first_large_enough(K, Fruits)
+    end.
+
+get_base({URLs, {I,_}}) -> array:get(I, URLs).
+
+get_conn({Conns, {I,_}}) -> array:get(I, Conns).
+
+get_path({struct, PL}, Path) ->
+    get_path(PL, Path);
+get_path(PL, [Name]) ->
+    case proplists:get_value(Name, PL) of
+        {struct, Obj} -> Obj;
+        Val -> Val
+    end;
+get_path(PL, [Name|Path]) ->
+    get_path(proplists:get_value(Name, PL), Path).
+
+make_url(Path) ->
+    fun({IP, Port}) -> ?FMT("http://~s:~w~s", [IP, Port, Path]) end.
+
+make_conn({IP, Port}) ->
+    {ok, Conn} = riakc_pb_socket:start_link(IP, Port),
+    Conn.
+
+http_get(URL) ->
+    case ibrowse:send_req(URL, [], get, [], [{response_format,binary}]) of
+        {ok, "200", _, Body} -> {ok, Body};
+        {ok, Status, _, _} -> {error, {bad_status, Status, URL}};
+        {error, Reason} -> {error, Reason}
+    end.
+
+http_put(URL, CT, Body) ->
+    case ibrowse:send_req(URL, [{content_type, CT}], put, Body, [{content_type, CT}]) of
+        {ok, "200", _, _} -> ok;
+        {ok, "201", _, _} -> ok;
+        {ok, "204", _, _} -> ok;
+        {ok, Status, _, Resp} -> {error, {bad_status, Status, URL, Resp}};
+        {error, Reason} -> {error, Reason}
+    end.
+
+wrap({URLs, {I,N}}) when I == N - 1 -> {URLs, {0,N}};
+wrap({URLs, {I,N}}) -> {URLs, {I+1,N}}.
---
+++ misc/basho_bench/src/yz_file_terms.erl
@@ -0,0 +1,146 @@
+%% @doc This server is used to generate {Field, Terms} pairs.  It is
+%%      given a file, a list of fileds, and a schema which it then
+%%      uses to iterate the file, line by line, extracting {Field,
+%%      Terms} pairs as they are requested.
+-module(yz_file_terms).
+-behavior(gen_server).
+-compile(export_all).
+
+%% Callbacks
+-export([init/1, handle_call/3, terminate/2]).
+
+-include_lib("basho_bench/include/basho_bench.hrl").
+-define(MB, 1048576).
+-record(state, {cache, i, fields, file, schema}).
+
+-type field() :: binary().
+-type sterm() :: binary().
+-type sterms() :: [sterm()].
+
+%% ====================================================================
+%% API
+%% ====================================================================
+
+%% @doc Return a {Field, Terms} pair.
+-spec get_ft() -> {field(), sterms()}.
+get_ft() -> get_ft(1).
+
+%% @doc Return a {Field, Terms} pair.  An attempt will be made to
+%% return N terms but could be less.
+-spec get_ft(pos_integer()) -> {field(), sterms()}.
+get_ft(N) -> gen_server:call(term_server, {ft, N}).
+
+get_line() -> gen_server:call(line_server, line).
+
+start_ts(Path, Fields, Schema) ->
+    gen_server:start_link({local, term_server}, ?MODULE,
+                          [Path, Fields, Schema], []).
+
+start_ls(Path) ->
+    gen_server:start_link({local, line_server}, ?MODULE, [Path], []).
+
+
+%% ====================================================================
+%% Callbacks
+%% ====================================================================
+
+%% -spec init([string(), [binary()], string()]) -> {ok, #state{}}.
+init([Path, Fields, SchemaPath]) ->
+    {ok, F} = file:open(Path, [read, raw, binary, {read_ahead, ?MB}]),
+    Schema = load_schema(SchemaPath),
+    {ok, #state{cache=[], fields=Fields, file=F, schema=Schema}};
+init([Path]) ->
+    {ok, F} = file:open(Path, [read, raw, binary, {read_ahead, ?MB}]),
+    {ok, #state{cache=[], i=1, file=F}}.
+
+handle_call({ft, N}, _From, S=#state{cache=[],
+                                     fields=Fields,
+                                     file=F,
+                                     schema=Schema}) ->
+    Pairs = read_pairs(F),
+    Pairs2 = lists:filter(match(Fields), Pairs),
+    Pairs3 = lists:map(analyze(Schema), Pairs2),
+    {Pair, Cache2} = get_terms(Pairs3, N),
+    {reply, Pair, S#state{cache=Cache2}};
+handle_call({ft, N}, _From, S=#state{cache=Cache}) ->
+    {Pair, Cache2} = get_terms(Cache, N),
+    {reply, Pair, S#state{cache=Cache2}};
+handle_call(line, _From, S=#state{cache=[], i=I, file=F}) ->
+    %% TODO no wrap around on line?
+    {[Line], Cache} = lists:split(1, read_lines(F, 10)),
+    Is = integer_to_list(I),
+    {reply, {Is, Line}, S#state{cache=Cache, i=I+1}};
+handle_call(line, _From, S=#state{cache=[Line|Cache], i=I}) ->
+    Is = integer_to_list(I),
+    {reply, {Is, Line}, S#state{cache=Cache, i=I+1}}.
+
+terminate(_Reason, _State) -> ignore.
+
+%% ====================================================================
+%% Private
+%% ====================================================================
+
+get_terms([{Field, Terms}|Pairs], N) ->
+    {T1, T2} = lists:split(N, Terms),
+    Pair = {Field, T1},
+    case length(T2) of
+        0 -> {Pair, Pairs};
+        _ -> {Pair, [{Field, T2}|Pairs]}
+    end.
+
+match(Fields) ->
+    S = sets:from_list(Fields),
+    fun({Field, _}) ->
+            sets:is_element(Field, S)
+    end.
+
+analyze(Schema) ->
+    fun({Field, Val}) ->
+            X = Schema:find_field(Field),
+            {erlang, M, F} = Schema:analyzer_factory(X),
+            A = Schema:analyzer_args(X),
+            {ok, Res} = M:F(Val, A),
+            {Field, Res}
+    end.
+
+load_schema(Path) ->
+    {ok, S1} = file:read_file(Path),
+    {ok, S2} = riak_search_utils:consult(S1),
+    {ok, S3} = riak_search_schema_parser:from_eterm(<<"test">>, S2),
+    S3.
+
+read_pairs(F) ->
+    read_pairs(F, 0).
+
+read_pairs(_F, 100) ->
+    %% Guard against infinite loop
+    throw({field_extraction, too_many_retries});
+
+read_pairs(F, Retry) ->
+    Line = read_line(F),
+
+    %% Guard against invalid JSON
+    try
+        riak_search_kv_json_extractor:extract_value(Line, default_field,
+                                                    ignored)
+    catch _:Reason ->
+            ?ERROR("Failed to extract line: ~p", [Reason]),
+            read_pairs(F, Retry+1)
+    end.
+
+read_lines(F, N) -> read_lines(F, N, []).
+
+read_lines(_F, 0, Lines) -> lists:reverse(Lines);
+read_lines(F, N, Lines) ->
+    Line = read_line(F),
+    read_lines(F, N-1, [Line|Lines]).
+
+read_line(F) ->
+    case file:read_line(F) of
+        {ok, Line} -> ok;
+        eof ->
+            file:position(F, bof),
+            {ok, Line} = file:read_line(F),
+            ok
+    end,
+    Line.
