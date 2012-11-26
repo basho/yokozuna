f45af321e5b368d75004af10247c8be5841f84c3
Ryan Zezeski <rzezeski@gmail.com>
2012-11-25 21:14:03 -0500

    Filter java procs

--- misc/bench/bin/smartos/pid-cpu-mem-collect.sh
+++ misc/bench/bin/smartos/pid-cpu-mem-collect.sh
@@ -8,7 +8,7 @@ output=/tmp/pid-cpu-mem-collect-raw.txt
 case $ACTION in
     start)
         pkill prstat
-        prstat -du -p "$(pgrep -d, 'beam|java')" 1 > $output
+        prstat -du -p "$(pgrep -d, -f 'beam|^[^ ]*java')" 1 > $output
         ;;
     stop)
         pkill prstat
