8a398afd5338cca83643f7a46d83c775b95a6823
Ryan Zezeski <rzezeski@gmail.com>
2013-03-06 22:31:36 -0500

    Add scripts to roll-up basho bench results
    I use this scripts to calcualte numbers for building roll-up plots.
    It's a lot easier to quickly compare the results of 8 benchmarks in
    one plot than it is to look at 8 plots.

---
+++ misc/bench/bin/calc-mean-thru.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Use to calculate the mean throughput for a particular run of basho
+# bench.
+#
+# ./calc-mean-thru.sh <results-dir>
+#
+# for d in yz-fruit-query-*; do ~/stories/solr-dist-search/plots/calc-mean-thru.sh $d; done
+
+RESULTS_DIR=$1
+
+echo -n "Mean ops/s for $RESULTS_DIR: "
+# Don't include the header, 1st result or last result
+sed -e '1,2d' -e '$d' $RESULTS_DIR/summary.csv | \
+    awk -F, 'NR != 1 { total += $4/$2 } END { printf("%f\n", total / NR) }'
---
+++ misc/bench/bin/calc-med-latency.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Use to calculate the median latency for a particular run of basho
+# bench.
+#
+# ./calc-mean-thru.sh <results-dir>
+#
+# for d in yz-fruit-query-*; do ~/stories/solr-dist-search/plots/calc-mean-thru.sh $d; done
+
+RESULTS_DIR=$1
+
+for lat in $RESULTS_DIR/*latencies*
+do
+    # taking average of all the medians, divide by 1000 to convert
+    # from microseconds to milli
+    echo -n "The mean median latency for $lat: "
+    sed -e '1,2d' -e '$d' $lat | \
+        awk -F, '{total += $6 } END { printf("%f\n", (total / NR) / 1000) }'
+done
