eb59e567bc85789aebc5a6018c6b017f7480ce25
Ryan Zezeski <rzezeski@gmail.com>
2013-08-27 17:56:23 -0400

    Fix AAE repair, hash object once
    Given the right conditions Yokozuna's AAE can enter a state where some
    keys are indefinitely repaired.  I.e. the same keys get repaired over
    and over.  This turned out to be many things coming together to cause
    the bug.
    
    * Use a ring of 16 and 3 nodes, causing node repetition in some
      preflists.
    
    * Two RPC calls, in the repair function, to same node using same
      process.
    
    * The use of the key `tree` for caching the hashtree pid in the
      process dictionary.
    
    Below is a copy of redbug output showing that `yz_kv:get_tree` was
    returning the same Pid for two difference partitions.  Ultimately what
    this all means is that the hashtree updates were going to the wrong
    hashtree and thus AAE kept repairing them.
    
    ```
    03:51:18 <{erlang,apply,2}> {yz_kv,get_tree,[0]}
    03:51:18 <{erlang,apply,2}> {yz_kv,get_tree,1} -> <0.1142.0>
    03:51:18 <{erlang,apply,2}> {yz_kv,get_tree,
                                       [1370157784997721485815954530671515330927436759040]}
    03:51:18 <{erlang,apply,2}> {yz_kv,get_tree,1} -> <0.1142.0>
    ```
    
    This commit fixes that by using `{tree,Partition}` as the key for
    uniqueness.
    
    I also change the repair function to perform a local get and re-index
    rather than cross node boundaries.  This means cheaper repair and no
    RPC calls.
    
    Finally, I made a change to where the object hash is calculated to
    avoid performing the computation twice.

--- src/yz_doc.erl
+++ src/yz_doc.erl
@@ -47,16 +47,16 @@ doc_id(O, Partition, Sibling) ->
 has_siblings(O) -> riak_object:value_count(O) > 1.
 
 %% @doc Given an object generate the doc to be indexed by Solr.
--spec make_docs(obj(), binary(), binary(), boolean()) -> [doc()].
-make_docs(O, FPN, Partition, IndexContent) ->
-    [make_doc(O, Content, FPN, Partition, IndexContent)
+-spec make_docs(obj(), hash(), binary(), binary(), boolean()) -> [doc()].
+make_docs(O, Hash, FPN, Partition, IndexContent) ->
+    [make_doc(O, Hash, Content, FPN, Partition, IndexContent)
      || Content <- riak_object:get_contents(O)].
 
--spec make_doc(obj(), {dict(), dict()}, binary(), binary(), boolean()) -> doc().
-make_doc(O, {MD, V}, FPN, Partition, IndexContent) ->
+-spec make_doc(obj(), hash(), {dict(), dict()}, binary(), binary(), boolean()) -> doc().
+make_doc(O, Hash, {MD, V}, FPN, Partition, IndexContent) ->
     Vtag = get_vtag(O, MD),
     DocId = doc_id(O, Partition, Vtag),
-    EntropyData = gen_ed(O, Partition),
+    EntropyData = gen_ed(O, Hash, Partition),
     Bkey = {yz_kv:get_obj_bucket(O), yz_kv:get_obj_key(O)},
     Fields = make_fields({DocId, Bkey, FPN,
                           Partition, Vtag, EntropyData}),
@@ -203,10 +203,9 @@ gen_ts() ->
 %% NOTE: All of this data needs to be in one field to efficiently
 %%       iterate.  Otherwise the doc would have to be fetched for each
 %%       entry.
-gen_ed(O, Partition) ->
+gen_ed(O, Hash, Partition) ->
     TS = gen_ts(),
     RiakBucket = yz_kv:get_obj_bucket(O),
     RiakKey = yz_kv:get_obj_key(O),
-    %% TODO: do this in KV vnode and pass to hook
-    Hash = base64:encode(yz_kv:hash_object(O)),
-    <<TS/binary," ",Partition/binary," ",RiakBucket/binary," ",RiakKey/binary," ",Hash/binary>>.
+    Hash64 = base64:encode(Hash),
+    <<TS/binary," ",Partition/binary," ",RiakBucket/binary," ",RiakKey/binary," ",Hash64/binary>>.
