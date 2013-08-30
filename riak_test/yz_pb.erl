cf968f442f666cabd6ff424c44fdd339e92ae658
Eric Redmond <eric.redmond@gmail.com>
2013-08-30 08:37:20 -0700

    Replace pb message testing with client driver

--- riak_test/yz_pb.erl
+++ riak_test/yz_pb.erl
@@ -62,32 +62,19 @@ create_index(Cluster, Index, Bucket) ->
     [{Host, Port}] = host_entries(rt:connection_info([Node])),
     lager:info("create_index ~s for bucket ~s [~p]", [Index, Bucket, {Host, Port}]),
     {ok, Pid} = riakc_pb_socket:start_link(Host, (Port-1)),
-    F = fun(_) ->
-            %% set index in props with the same name as the bucket
-            Idx     = #rpbyokozunaindex{name = Index},
-            PutReq  = #rpbyokozunaindexputreq{index = Idx, schema = <<>>},
-            PutResp = gen_server:call(Pid, {req, PutReq, infinity}, infinity),
-            %% There is currently no admin driver impl in the erlang client
-            %% so capture the process_response error and check the reply
-            {error,{unknown_response,_,PutRespMsg}} = PutResp,
-            ?assertEqual(rpbputresp, PutRespMsg),
+    % F = fun(_) ->
+    %% set index in props with the same name as the bucket
+    ?assertEqual(ok, riakc_pb_socket:create_search_index(Pid, Idx)),
 
-            % Add the index to the bucket props
-            yz_rt:set_index(Node, Index, Bucket),
+    % Add the index to the bucket props
+    yz_rt:set_index(Node, Index, Bucket),
+    yz_rt:wait_for_index(Cluster, binary_to_list(Index)),
 
-            yz_rt:wait_for_index(Cluster, binary_to_list(Index)),
-
-            %% Check that the index exists
-            GetReq  = #rpbyokozunaindexgetreq{name = Index},
-            GetResp = gen_server:call(Pid, {req, GetReq, infinity}, infinity),
-            {error,{unknown_response,_,GetRespMsg}} = GetResp,
-
-            {rpbyokozunaindexgetresp,GestRespIndexes} = GetRespMsg,
-            [{rpbyokozunaindex,Index,Schema}] = GestRespIndexes,
-
-            ?YZ_DEFAULT_SCHEMA_NAME =:= Schema
-        end,
-    yz_rt:wait_until(Cluster, F),
+    %% Check that the index exists
+    {ok, IndexData} = riakc_pb_socket:get_search_index(Pid, Index),
+    ?assertEqual([{index,Index},{schema,?YZ_DEFAULT_SCHEMA_NAME}], IndexData),
+    %     end,
+    % yz_rt:wait_until(Cluster, F),
     riakc_pb_socket:stop(Pid),
     ok.
 
@@ -125,19 +112,7 @@ confirm_admin_schema(Cluster) ->
     [{Host, Port}] = host_entries(rt:connection_info([Node])),
     lager:info("confirm_admin_schema ~s [~p]", [Schema, {Host, Port}]),
     {ok, Pid} = riakc_pb_socket:start_link(Host, (Port-1)),
-    F = fun(_) ->
-        %% set index in props with the same name as the bucket
-        Sch     = #rpbyokozunaschema{name = Schema},
-        PutReq  = #rpbyokozunaschemaputreq{schema = Sch},
-        PutResp = gen_server:call(Pid, {req, PutReq, infinity}, infinity),
-        %% There is currently no admin driver impl in the erlang client
-        %% so capture the process_response error and check the reply
-        {error,{unknown_response,_,PutRespMsg}} = PutResp,
-        ?assertEqual(rpbputresp, PutRespMsg),
-
-        % true
-        rpbputresp =:= PutRespMsg
-    end,
+    ?assertEqual(ok, riakc_pb_socket:create_search_schema(Pid, Schema)),
     yz_rt:wait_until(Cluster, F),
     riakc_pb_socket:stop(Pid),
     ok.
@@ -149,12 +124,11 @@ confirm_admin_index(Cluster) ->
     [{Host, Port}] = host_entries(rt:connection_info([Node])),
     {ok, Pid} = riakc_pb_socket:start_link(Host, (Port-1)),
     F = fun(_) ->
-        %% Remove index from bucket props
+        %% Remove index from bucket props and delete it
         yz_rt:set_index(Node, Index, <<>>),
-        DelReq  = #rpbyokozunaindexdeletereq{name = Index},
-        DelResp = gen_server:call(Pid, {req, DelReq, infinity}, infinity),
+        DelResp = riakc_pb_socket:delete_search_index(Pid, Index)),
         case DelResp of
-            {error,{unknown_response,_,rpbdelresp}} -> true;
+            ok -> true;
             {error,<<"notfound">>} -> true
         end
     end,
