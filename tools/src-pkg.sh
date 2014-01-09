4252c3b2c416fd7ddd02787712f13ca36ce54a67
rzezeski <rzezeski@gmail.com>
2014-01-09 07:12:25 +0000

    Update src-pkg.sh to work with make dist
    Use Riak's dist target so that all the .git dirs are removed from the
    source package making it much smaller in size.

--- tools/src-pkg.sh
+++ tools/src-pkg.sh
@@ -8,9 +8,10 @@
 ##
 ## EXAMPLE
 ##
-##    ./src-pkg.sh /tmp v0.3.0
+##    ./src-pkg.sh /tmp 0.3.0
 ##
-##    ./src-pkg.sh -rt riak-2.0.0pre9 /tmp v0.13.0
+##    ./src-pkg.sh -rt riak-2.0.0pre9 /tmp 0.13.0
+set -e
 
 usage() {
     echo
@@ -53,21 +54,40 @@ VSN=$1
 RIAK_DIR=riak-yokozuna-$VSN-src
 TGZ=$RIAK_DIR.tar.gz
 
-pushd $WD
-git clone git://github.com/basho/riak.git $RIAK_DIR
-pushd $RIAK_DIR
+pushd $WD                       # in working dir
+
+if [ ! -d $RIAK_DIR ]; then
+    git clone git://github.com/basho/riak.git $RIAK_DIR
+fi
+
+pushd $RIAK_DIR                 # in riak
+
 if [ -n $riak_tag ]
 then
     git checkout $riak_tag
 fi
-make deps
-pushd deps/yokozuna
-git checkout v$VSN
+
+if [ -n $riak_tag ]
+then
+    cp rebar.config.lock rebar.config
+fi
+
+sed -i"bak" -e "/{yokozuna.*/{
+N
+N
+s#{yokozuna.*#{yokozuna, \".*\", {git, \"git://github.com/basho/yokozuna.git\", {tag, \"v${VSN}\"}}},#
+}" rebar.config
+
+git commit -am "Checkout Yokozuna version v$VSN"
+
+make PKG_ID="$RIAK_DIR" dist
+
+tar zxvf $TGZ
+pushd $RIAK_DIR/deps/yokozuna   # in extracted pkg
 ./tools/grab-solr.sh
 rm -rf build/solr-4*
-popd
-popd
-
-rm -f $TGZ
+popd                            # out extracted pkg
 tar -zcvf $TGZ $RIAK_DIR
-popd
+
+popd                            # out riak
+popd                            # out working dir
