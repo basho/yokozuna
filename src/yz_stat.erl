aad62c4938b7d060d2bd3a72a4a46d985c7ecd70
Jon Anderson <janderson@basho.com>
2014-02-13 10:37:13 -0500

    renamed yz stats to search/[index|query] instead of yokozuna/[search|index|

--- src/yz_stat.erl
+++ src/yz_stat.erl
@@ -195,62 +195,64 @@ format_stats(Stats) ->
     lists:flatten([format(Stat) || Stat <- Stats]).
 
 format({{_, search, fail},[{count, Count}, {one, One}]}) ->
-    [{yokozuna_search_fail_count, Count},
-     {yokozuna_search_fail_one, One}];
+    [{search_query_fail_count, Count},
+     {search_query_fail_one, One}];
 format({{_, search, throughput},[{count, Count}, {one, One}]}) ->
-    [{yokozuna_search_throughput_count, Count},
-     {yokozuna_search_throughput_one, One}];
+    [{search_query_throughput_count, Count},
+     {search_query_throughput_one, One}];
 format({{_, index, fail},[{count, Count}, {one, One}]}) ->
-    [{yokozuna_index_fail_count, Count},
-     {yokozuna_index_fail_one, One}];
+    [{search_index_fail_count, Count},
+     {search_index_fail_one, One}];
 format({{_, index, throughput},[{count, Count}, {one, One}]}) ->
-    [{yokozuna_index_fail_count, Count},
-     {yokozuna_index_fail_one, One}];
+    [{search_index_throughput_count, Count},
+     {search_index_throughput_one, One}];
 format({{_, search, latency}, ValueList}) ->
-    [format_search_latency(V) || V <- ValueList];
+    [format_query_latency(V) || V <- ValueList];
 format({{_, index, latency}, ValueList}) ->
     [format_index_latency(V) || V <- ValueList];
+format({yokozuna_stat_ts, TS}) ->
+    [{search_stat_ts, TS}];
 format(T) -> T.
 
-format_search_latency({min, Value}) ->
-    {yokozuna_search_latency_min, Value};
-format_search_latency({max, Value}) ->
-    {yokozuna_search_latency_max, Value};
-format_search_latency({median, Value}) ->
-    {yokozuna_search_latency_median, Value};
-format_search_latency({variance, Value}) ->
-    {yokozuna_search_latency_variance, Value};
-format_search_latency({percentile, PList}) ->
-    [ format_search_latency_percentile(P) || P <- PList];
-format_search_latency({_, _}) ->
+format_query_latency({min, Value}) ->
+    {search_query_latency_min, Value};
+format_query_latency({max, Value}) ->
+    {search_query_latency_max, Value};
+format_query_latency({median, Value}) ->
+    {search_query_latency_median, Value};
+format_query_latency({variance, Value}) ->
+    {search_query_latency_variance, Value};
+format_query_latency({percentile, PList}) ->
+    [ format_query_latency_percentile(P) || P <- PList];
+format_query_latency({_, _}) ->
     [].
 
-format_search_latency_percentile({50, Value}) ->
-    {yokozuna_search_latency_percentile_50, Value};
-format_search_latency_percentile({95, Value}) ->
-    {yokozuna_search_latency_percentile_95, Value};
-format_search_latency_percentile({99, Value}) ->
-    {yokozuna_search_latency_percentile_99, Value};
-format_search_latency_percentile(_) -> [].
+format_query_latency_percentile({50, Value}) ->
+    {search_query_latency_percentile_50, Value};
+format_query_latency_percentile({95, Value}) ->
+    {search_query_latency_percentile_95, Value};
+format_query_latency_percentile({99, Value}) ->
+    {search_query_latency_percentile_99, Value};
+format_query_latency_percentile(_) -> [].
 
 format_index_latency({min, Value}) ->
-    {yokozuna_index_latency_min, Value};
+    {search_index_latency_min, Value};
 format_index_latency({max, Value}) ->
-    {yokozuna_index_latency_max, Value};
+    {search_index_latency_max, Value};
 format_index_latency({median, Value}) ->
-    {yokozuna_index_latency_median, Value};
+    {search_index_latency_median, Value};
 format_index_latency({variance, Value}) ->
-    {yokozuna_index_latency_variance, Value};
+    {search_index_latency_variance, Value};
 format_index_latency({percentile, PList}) ->
     [ format_index_latency_percentile(P) || P <- PList];
 format_index_latency({_, _}) ->
     [].
 
 format_index_latency_percentile({50, Value}) ->
-    {yokozuna_index_latency_percentile_50, Value};
+    {search_index_latency_percentile_50, Value};
 format_index_latency_percentile({95, Value}) ->
-    {yokozuna_index_latency_percentile_95, Value};
+    {search_index_latency_percentile_95, Value};
 format_index_latency_percentile({99, Value}) ->
-    {yokozuna_index_latency_percentile_99, Value};
+    {search_index_latency_percentile_99, Value};
 format_index_latency_percentile(_) -> [].
 
