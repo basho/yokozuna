0547aa1b34b15e4ab3b80837eca8d2d3a8a5c34d
Jon Anderson <janderson@basho.com>
2014-03-07 17:14:51 -0500

    addressed review feedback

--- src/yz_cover.erl
+++ src/yz_cover.erl
@@ -149,55 +149,18 @@ calc_plan(Index, Ring) ->
     NVal = yz_index:get_n_val(yz_index:get_index_info(Index)),
     CoveragePlan = create_coverage_plan(NVal),
     maybe_filter_plan(CoveragePlan, Ring, NVal, NumPartitions).
-
-%% @private
-%%
-%% @doc Filter plan or error
--spec maybe_filter_plan(term(), ring(), integer(), integer()) ->  {ok, plan()} | {error, term()}.
-maybe_filter_plan({error, Error}, _, _, _) ->
-    Error;
-maybe_filter_plan({CoverSet, _}, Ring, NVal, NumPartitions) ->
-    LogicalCoverSet = calc_plan_add_filtering(CoverSet, Ring, NVal, NumPartitions),
-    UniqNodes = get_uniq_nodes(CoverSet),
-    Mapping = yz_solr:build_mapping(UniqNodes),
-    calc_plan_return(length(Mapping) == length(UniqNodes), UniqNodes, LogicalCoverSet, Mapping).
-
-%% @private
-%%
-%% @doc handle the result of the calculation result with the 
-%%      result of filtering
--spec calc_plan_return(true | false, list(), logical_cover_set(), list()) ->  {ok, plan()} | {error, term()}.
-calc_plan_return(false, _, _, _) ->
-    {error, "Failed to determine Solr port for all nodes in search plan"};
-calc_plan_return(true, UniqNodes, LogicalCoverSet, Mapping) ->
-    {ok, {UniqNodes, LogicalCoverSet, Mapping}}.
-
-%% @private
-%%
-%% @doc return unique list of nodes
--spec get_uniq_nodes(logical_cover_set()) -> list().
-get_uniq_nodes(CoverSet) ->
-    {_Partitions, Nodes} = lists:unzip(CoverSet),
-    lists:usort(Nodes).
-
-%% @private
-%%
-%%  @doc add filtering with the NVal, NumPartitionsand CoverSet and return
-%% 
--spec calc_plan_add_filtering(logical_cover_set(), ring(), integer(), integer()) -> logical_cover_set().
-calc_plan_add_filtering(CoverSet, Ring, NVal, NumPartitions) ->
-    LPI = logical_index(Ring),
-    add_filtering(NVal, NumPartitions, LPI, CoverSet).
     
 %% @private
 %%
-%% @doc create coverage plan
--spec create_coverage_plan(integer()) -> term().
+%% @doc Create a Riak core coverage plan.
+-spec create_coverage_plan(n()) -> term().
 create_coverage_plan(NVal) ->
     ReqId = erlang:phash2(erlang:now()),
-    riak_core_coverage_plan:create_plan(all,
+    NumPrimaries = 1,
+    Selector=all,
+    riak_core_coverage_plan:create_plan(Selector,
                                         NVal,
-                                        1,
+                                        NumPrimaries,
                                         ReqId,
                                         ?YZ_SVC_NAME).
 
@@ -211,6 +174,14 @@ get_distance(Q, {LPA,_}, {LPB,_}) when LPB < LPA ->
     BottomDiff + TopDiff + 1;
 get_distance(_Q, {LPA,_}, {LPB,_}) ->
     LPB - LPA.
+
+%% @private
+%%
+%% @doc This function converts CovertSet into logical partitions and adds filtering information.
+-spec get_uniq_nodes(logical_cover_set()) -> [node()].
+get_uniq_nodes(CoverSet) ->
+    {_Partitions, Nodes} = lists:unzip(CoverSet),
+    lists:usort(Nodes).
 
 %% @doc Create a mapping from logical to actual partition.
 -spec logical_index(riak_core_ring:riak_core_ring()) -> logical_idx().
@@ -273,11 +244,40 @@ make_cover_set(N, Q, Cover) ->
 make_logical(LogicalIndex, PSet) ->
     [{logical_partition(LogicalIndex, P), Node} || {P, Node} <- PSet].
 
+%% @private
+%%
+%% @doc Add filtering to the cover set and return a logical cover set.
+-spec make_logical_and_filter(logical_cover_set(), ring(), integer(), integer()) -> logical_cover_set().
+make_logical_and_filter(CoverSet, Ring, NVal, NumPartitions) ->
+    LPI = logical_index(Ring),
+    add_filtering(NVal, NumPartitions, LPI, CoverSet).
+
+%% @private
+%%
+%% @doc Filter plan or return error.
+-spec maybe_filter_plan(term(), ring(), n(), pos_integer()) ->  {ok, plan()} | {error, term()}.
+maybe_filter_plan({error, Error}, _, _, _) ->
+    {error, Error};
+maybe_filter_plan({CoverSet, _}, Ring, NVal, NumPartitions) ->
+    LogicalCoverSet = make_logical_and_filter(CoverSet, Ring, NVal, NumPartitions),
+    UniqNodes = get_uniq_nodes(CoverSet),
+    Mapping = yz_solr:build_mapping(UniqNodes),
+    plan_return(length(Mapping) == length(UniqNodes), UniqNodes, LogicalCoverSet, Mapping).
+
 %% @doc Map `LP' to actual partition.
 -spec partition(logical_idx(), lp()) -> p().
 partition(LogicalIndex, LP) ->
     {_, P} = lists:keyfind(LP, 1, LogicalIndex),
     P.
+
+%% @private
+%%
+%% @doc Handle the result of the calculation result with the result of filtering.
+-spec plan_return(boolean(), [node()], logical_cover_set(), list()) ->  {ok, plan()} | {error, term()}.
+plan_return(false, _, _, _) ->
+    {error, "Failed to determine Solr port for all nodes in search plan"};
+plan_return(true, UniqNodes, LogicalCoverSet, Mapping) ->
+    {ok, {UniqNodes, LogicalCoverSet, Mapping}}.
 
 %% @private
 %%
