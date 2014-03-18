b9a3deb18cb8dea835f09fccf4ec9af1c9b2ccae
Jon Anderson <janderson@basho.com>
2014-03-18 09:37:31 -0400

    Add further property tests for yz_misc.
    - Test for yz_misc:hostname/1.
    - Test for yz_mist:get_dict/3.

--- test/yz_utils_eqc.erl
+++ test/yz_utils_eqc.erl
@@ -25,10 +25,15 @@
 %%====================================================================
 
 eqc_test_() ->
-    Time = 8,
+    Time = 2,
     [
      {timeout, Time*4, ?_assertEqual(true,
-                                     eqc:quickcheck(eqc:testing_time(Time,?QC_OUT(prop_compress()))))}
+                                     eqc:quickcheck(eqc:testing_time(Time,?QC_OUT(prop_compress()))))},
+     {timeout, Time*4, ?_assertEqual(true,
+                                     eqc:quickcheck(eqc:testing_time(Time,?QC_OUT(prop_hostname()))))},
+     {timeout, Time*4, ?_assertEqual(true,
+                                     eqc:quickcheck(eqc:testing_time(Time,?QC_OUT(prop_dict()))))}
+
     ].
 
 %% ====================================================================
@@ -39,6 +44,28 @@ prop_compress() ->
     ?FORALL(S, utf8_string(),
             yz_misc:decompress(yz_misc:compress(S)) == [S]).
 
+prop_hostname() ->
+    ?FORALL({Node, Host}, gen_nodes(),
+            (yz_misc:hostname(Node) == atom_to_list(Host))).
+
+prop_dict() ->
+    ?FORALL({Key, Dict, Val}, 
+            {non_blank_string(), gen_dict(), non_blank_string()},
+            begin
+                 NewDict = dict:store(Key, Val, Dict),
+                 yz_misc:dict_get(Key, NewDict, <<>>) =:= Val
+            end).
+
+%%====================================================================
+%% Generators
+%%====================================================================
+
+non_blank_string() ->
+    ?LET(X, not_empty(list(lower_char())), list_to_binary(X)).
+
+lower_char() ->
+    choose(16#20, 16#7f).
+
 utf8_string() ->
     ?LET(X, not_empty(list(utf8_char())), unicode:characters_to_binary(X)).
 
@@ -48,8 +75,11 @@ not_empty(G) ->
 utf8_char() ->
     choose(16#0000, 16#00FF).
 
-%%====================================================================
-%% Helpers
-%%====================================================================
+gen_nodes() ->
+    oneof([{'foo@127.0.0.1', '127.0.0.1'}, {'baz@localhost', 'localhost'}, 
+           {'bat@bozeman-inc.com', 'bozeman-inc.com'}, {'eddie-vedder@123.32.23.123', '123.32.23.123'}]).
 
+gen_dict() ->
+    dict:new().
+    
 -endif.
