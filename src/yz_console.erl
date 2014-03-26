dba5789f876ae16d387e3513c830acfa962c14cd
rzezeski <rzezeski@gmail.com>
2014-03-26 21:03:01 +0000

    Add switch-to-new-search command
    Add the switch-to-new-search command under the search command. This is
    used when migrating from legacy riak_search to yokozuna. It does a
    multicall across the cluster handing over HTTP and PB control from
    riak_search to yokozuna.

--- src/yz_console.erl
+++ src/yz_console.erl
@@ -19,7 +19,8 @@
 %% -------------------------------------------------------------------
 -module(yz_console).
 -include("yokozuna.hrl").
--export([aae_status/1]).
+-export([aae_status/1,
+         switch_to_new_search/1]).
 
 %% @doc Print the Active Anti-Entropy status to stdout.
 -spec aae_status([]) -> ok.
@@ -32,3 +33,21 @@ aae_status([]) ->
     io:format("~n"),
     riak_kv_console:aae_repair_status(ExchangeInfo),
     ok.
+
+%% @doc Switch over HTTP solr route and PB handling from legacy
+%% riak_search to yokozuna. This will multicall to all nodes in the
+%% cluster. If any nodes are down report them to stderr and return an
+%% error tuple. Once the switch is made the system cannot be switched
+%% back without restarting the cluster.
+-spec switch_to_new_search([]) -> ok | {error, {nodes_down, [node()]}}.
+switch_to_new_search([]) ->
+    {_Good, Down} = riak_core_util:rpc_every_member_ann(yokozuna, switch_to_yokozuna, [], 5000),
+    case Down of
+        [] ->
+            ok;
+        _ ->
+            Down2 = [atom_to_list(Node) || Node <- Down],
+            DownStr = string:join(Down2, " "),
+            io:format(standard_error, "The following nodes could not be reached: ~s", [DownStr]),
+            {error, {nodes_down, Down}}
+    end.
