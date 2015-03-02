ce4a8bbbc860926e90e76e926b828c654946e1c5
Bishop Bors <borshop@basho.com>
2015-03-02 15:36:19 -0700

    Merge pull request #458 from basho/bugfix/im/handle-get-ring-timeouts
    Bugfix/im/handle get ring timeouts
    
    Reviewed-by: aberghage
    (cherry picked from commit 32f8c6eb7d8cac6c80f798d7173b8517691c6caf)

--- src/yz_events.erl
+++ src/yz_events.erl
@@ -180,6 +180,7 @@ remove_non_owned_data(unknown) ->
     %% The ring used to calculate the current coverage plan could not
     %% be determined. In this case do nothing to prevent removing data
     %% that the current coverage plan is using.
+    ?DEBUG("the current ring is unknown, no data can be removed", []),
     ok;
 remove_non_owned_data(Ring) ->
     case yz_solr:cores() of
