eeb90d057781463606e378bda4c4373610d1fe2f
Fred Dushin <fdushin@basho.com>
2015-07-31 10:07:40 -0400

    After review, modified yz integration so that we preserve the old yz_kv:index/3 for existing indexing commands, but we add a new yz_kv:index_binary/5 for indexing binary encoded Riak Objects.  This requires that we also pass in a bucket and key, so that the riak object can be decoded.

--- test/yz_component_tests.erl
+++ test/yz_component_tests.erl
@@ -6,7 +6,7 @@
 
 disable_index_test()->
   yokozuna:disable(index),
-  ?assertEqual(yz_kv:index(dummy, dummy, {},delete,{}), ok).
+  ?assertEqual(yz_kv:index(riak_object:new({<<"type">>, <<"bucket">>}, <<"key">>, <<"value">>), delete, {}), ok).
 
 disable_search_test()->
     yokozuna:disable(search),
