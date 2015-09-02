521acd52cdf7111c7b7dfc98b4bac10e3f97d91f
Doug Rohrer <drohrer@basho.com>
2015-09-02 13:34:01 -0400

    WIP - Batching Solr calls

--- src/yz_solrq_helper.erl
+++ src/yz_solrq_helper.erl
@@ -58,27 +58,91 @@ init([QPid]) ->
     {ok, #state{qpid = QPid, http_pid = HttpPid}}.
 
 handle_call(status, _From, #state{qpid = QPid,
-                           http_pid = HttpPid}) ->
+                                  http_pid = HttpPid}) ->
     {reply, [{qpid, QPid},
              {http_pid, HttpPid}]};
 handle_call(BadMsg, _From, State) ->
     {reply, {error, {unknown, BadMsg}}, State}.
 
-handle_cast({entries, Entries}, #state{qpid = QPid,
+handle_cast({entries, Entries0}, #state{qpid = QPid,
                                        http_pid = _HttpPid} = State) ->
     try
         %% TODO: use ibrowse http worker
-        %% TODO: batch updates to SOLR
         %% TODO: batch updates to YZ AAE
-        _ = [index(Index, BKey, Obj, Reason, P) ||
-                {Index, BKey, Obj, Reason, P} <- Entries],
+        Ring = yz_misc:get_ring(transformed),
+        T1 = os:timestamp(),
+        Entries = [{Index, BKey, Obj, Reason, P, riak_kv_util:get_index_n(BKey), yz_kv:hash_object(Obj)} ||
+            {Index, BKey, Obj, Reason, P} <- Entries0],
+        SolrUpdates = update_solr(Entries, Ring),
+        update_aae(SolrUpdates, Entries),
+        %% TODO: Since this is a batch now, do we need to do something different with the stat?
+        yz_stat:index_end(?YZ_TIME_ELAPSED(T1)),
         yz_solrq:poll(QPid, self()),
         {noreply, State}
     catch
         _:Err ->
-            lager:info("index failed - ~p\n", [Err]),
+            Trace = erlang:get_stacktrace(),
+            lager:info("index failed - ~p\nat: ~p", [Err, Trace]),
             {noreply, State}
     end.
+
+%% Entries is [{Index, BKey, Obj, Reason, P, ShortPL, Hash}]
+update_solr(Entries, Ring) ->
+    LI = yz_cover:logical_index(Ring),
+    OpsByIndex =
+        lists:foldl(fun({Index, BKey, Obj, Reason, P, ShortPL, Hash}, OpsByIndex0) ->
+            case yz_kv:is_owner_or_future_owner(P, node(), Ring) of
+                true ->
+                    EncodedOps = case Reason of
+                        delete ->
+                            [{delete, yz_solr:encode_delete([{bkey, BKey}])}];
+                        _ ->
+                            LFPN = yz_cover:logical_partition(LI, element(1, ShortPL)),
+                            LP = yz_cover:logical_partition(LI, P),
+                            AddOps = yz_doc:make_docs(Obj, Hash, ?INT_TO_BIN(LFPN), ?INT_TO_BIN(LP)),
+                            DeleteOps = yz_kv:delete_operation(Obj, Reason, AddOps, BKey, LP),
+                            [{delete, yz_solr:encode_delete(DeleteOp)} || DeleteOp <- DeleteOps] ++
+                            [{add, yz_solr:encode_doc(Doc)} || Doc <- AddOps]
+                    end,
+                    dict:merge(
+                        %% TODO: Could remove duplicat add ops here?
+                        fun(_Key, OldOps, NewOps) -> OldOps ++ NewOps end,
+                        OpsByIndex0,
+                        dict:from_list([{Index, EncodedOps}]));
+                _ -> OpsByIndex0
+            end
+            end,
+            dict:new(),
+            Entries),
+    send_solr_ops(dict:to_list(OpsByIndex)).
+
+send_solr_ops(OpsByIndex) ->
+    lists:map(fun({Index, Ops}) ->
+        try
+            yz_solr:index_batch(Index, Ops),
+            {Index, ok}
+        catch _:Err ->
+            yz_stat:index_fail(),
+            Trace = erlang:get_stacktrace(),
+            {Index, Err, Trace}
+        end
+    end, OpsByIndex).
+
+
+update_aae(SolrResults, Entries) ->
+    lists:foreach(fun({Index, BKey, _Obj, Reason0, P, ShortPL, Hash}) ->
+        Reason = case Reason0 of
+            delete -> delete;
+            _ -> insert
+        end,
+        case proplists:get_value(Index, SolrResults) of
+            ok ->
+                yz_kv:update_hashtree({Reason, Hash}, P, ShortPL, BKey);
+            {_Err, _Trace} ->
+                %% TODO: What do we do with a failed index batch? Index docs individually again?
+                ok
+        end
+        end, Entries).
 
 handle_info(_Msg, State) ->
     {noreply, State}.
@@ -88,37 +152,3 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
-
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
-
-index(Index, BKey, Obj, Reason, P) ->
-    Ring = yz_misc:get_ring(transformed),
-    case yz_kv:is_owner_or_future_owner(P, node(), Ring) of
-        true ->
-            T1 = os:timestamp(),
-            try
-                ShortPL = riak_kv_util:get_index_n(BKey),
-                case yz_kv:should_index(Index) of
-                    true ->
-                        yz_kv:index(Obj, Reason, Ring, P, BKey, ShortPL, Index);
-                    false ->
-                        yz_kv:dont_index(Obj, Reason, P, BKey, ShortPL)
-                end,
-                yz_stat:index_end(?YZ_TIME_ELAPSED(T1))
-            catch _:Err ->
-                    yz_stat:index_fail(),
-                    Trace = erlang:get_stacktrace(),
-                    case Reason of
-                        delete ->
-                            ?ERROR("failed to delete docid ~p with error ~p because ~p",
-                                   [BKey, Err, Trace]);
-                        _ ->
-                            ?ERROR("failed to index object ~p with error ~p because ~p",
-                                   [BKey, Err, Trace])
-                    end
-            end;
-        false ->
-            ok
-    end.
