3b59ef2964faeb2b208600f735e216d3291c1b87
Jon Meredith <jmeredith@basho.com>
2015-09-03 11:46:06 -0600

    Added resize, set_hwm and set_batch calls for solrq.
    Also increased default to 100 queues.

--- src/yz_solrq_sup.erl
+++ src/yz_solrq_sup.erl
@@ -20,7 +20,7 @@
 
 -behaviour(supervisor).
 
--export([start_link/0, regname/1]).
+-export([start_link/0, regname/1, resize/1, set_hwm/1, set_batch/2]).
 -export([init/1]).
 
 -define(SOLRQS_TUPLE_KEY, solrqs_tuple).
@@ -44,6 +44,43 @@ regname(Hash) ->
             element(Index, Names)
     end.
 
+%% Resize the number of queues.  For debugging/testing only,
+%% this will briefly cause the worker that queues remap to
+%% to change so updates may be out of order briefly.
+resize(NewSize) when NewSize > 0 ->
+    OldSize = supervisor:count_children(?MODULE),
+    %% Shrink to single worker while we mess with the
+    %% running workers
+    mochiglobal:put(?SOLRQS_TUPLE_KEY, solrqs_tuple(1)),
+    Result =
+        case NewSize of
+            OldSize ->
+                same_size;
+            NewSize when NewSize < OldSize ->
+                _ = [begin
+                         Name = int_to_regname(I),
+                         _ = supervisor:terminate_child(Name),
+                         ok = supervisor:delete_child(Name)
+                     end || I <- lists:seq(NewSize + 1, OldSize)],
+                {shrank, OldSize - NewSize};
+            NewSize when NewSize > OldSize ->
+                [supervisor:start_child(make_child(int_to_regname(I))) ||
+                    I <- lists:seq(OldSize + 1, NewSize)],
+                {grew, NewSize - OldSize}
+        end,
+    mochiglobal:put(?SOLRQS_TUPLE_KEY, solrqs_tuple(NewSize)),
+    Result.
+
+set_hwm(HWM) ->
+    [{Name, catch yz_solrq:set_hwm(Name, HWM)} ||
+        Name <- tuple_to_list(mochiglobal:get(?SOLRQS_TUPLE_KEY))].
+
+set_batch(Min, Max) ->
+    [{Name, catch yz_solrq:set_batch(Name, Min, Max)} ||
+        Name <- tuple_to_list(mochiglobal:get(?SOLRQS_TUPLE_KEY))].
+
+
+
 %%%===================================================================
 %%% Supervisor callbacks
 %%%===================================================================
@@ -56,8 +93,7 @@ regname(Hash) ->
 init([]) ->
     SolrQs = solrqs_tuple(),
     mochiglobal:put(?SOLRQS_TUPLE_KEY, SolrQs),
-    Children = [{Name, {yz_solrq, start_link, [Name]},
-                 permanent, 5000, worker, [yz_solrq]} ||
+    Children = [make_child(Name) ||
                    Name <- tuple_to_list(SolrQs)],
     {ok, {{one_for_one, 10, 10}, Children}}.
 
@@ -66,8 +102,14 @@ init([]) ->
 %%%===================================================================
 
 solrqs_tuple() ->
-    NumSolrQ =application:get_env(yokozuna, num_solrq, 10),
+    NumSolrQ =application:get_env(yokozuna, num_solrq, 100),
+    solrqs_tuple(NumSolrQ).
+
+solrqs_tuple(NumSolrQ) ->
     list_to_tuple([int_to_regname(I) || I <- lists:seq(1, NumSolrQ)]).
 
 int_to_regname(I) ->
     list_to_atom(lists:flatten(io_lib:format("yz_solrq_~4..0b", [I]))).
+
+make_child(Name) ->
+    {Name, {yz_solrq, start_link, [Name]}, permanent, 5000, worker, [yz_solrq]}.
