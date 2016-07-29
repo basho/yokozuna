7b45b70f599019d9756a23133d8a01ac971cc7da
Ted Burghart <ted@tedb.net>
2016-07-29 09:55:14 -0400

    check whether search jobs are allowed

--- src/yz_pb_search.erl
+++ src/yz_pb_search.erl
@@ -1,8 +1,6 @@
 %% -------------------------------------------------------------------
 %%
-%% yz_pb_search: PB Service for Yokozuna queries
-%%
-%% Copyright (c) 2013 Basho Technologies, Inc.  All Rights Reserved.
+%% Copyright (c) 2013-2016 Basho Technologies, Inc.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
@@ -59,7 +57,12 @@ encode(Message) ->
 
 %% @doc process/2 callback. Handles an incoming request message.
 process(Msg, State) ->
-    maybe_process(yokozuna:is_enabled(search), Msg, State).
+    case riak_core_util:job_class_enabled(riak_search) of
+        true ->
+            maybe_process(yokozuna:is_enabled(search), Msg, State);
+        _ ->
+            {error, "Operation 'riak_search' is not enabled", State}
+    end.
 
 maybe_process(true, #rpbsearchqueryreq{index=Index}=Msg, State) ->
     case extract_params(Msg) of
