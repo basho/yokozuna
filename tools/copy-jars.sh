119e30fbf4aff3f695f1ac622be34c3beec27b10
Antonio Nikishaev <me@lelf.lu>
2018-07-10 22:04:24 +0400

    tooling

--- tools/copy-jars.sh
+++ tools/copy-jars.sh
@@ -12,8 +12,6 @@ if [ ! -f $JAVA_LIB/$YZ_JAR_NAME ] || [ ${FORCE_REBUILD:-0} -gt 0 ]
 then
     mkdir -p $JAVA_LIB
     echo "Downloading $YZ_JAR_NAME"
-    # download "${ARTIFACT_URL_PREFIX}/yokozuna/$YZ_JAR_NAME"
-    download ${YZ_ARTIFACTS_URL}/${YZ_JAR_NAME}
     cp -v $YZ_JAR_NAME $JAVA_LIB
 fi
 
@@ -22,8 +20,6 @@ EXT_LIB=../priv/solr/lib/ext
 if [ ! -f $EXT_LIB/$MON_JAR_NAME ] || [ ${FORCE_REBUILD:-0} -gt 0 ]
 then
     echo "Downloading $MON_JAR_NAME"
-    #download "${ARTIFACT_URL_PREFIX}/yokozuna/$MON_JAR_NAME"
-    download ${YZ_ARTIFACTS_URL}/${MON_JAR_NAME}
     cp -v $MON_JAR_NAME $EXT_LIB
 fi
 
